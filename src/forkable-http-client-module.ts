ad19e09376b09911c96582ab68da3a798b638d18
Daan Scheerens <d.scheerens@gmail.com>
2017-11-05 23:05:02 +0100

    Removed obsolete code from the module file

--- src/forkable-http-client-module.ts
+++ src/forkable-http-client-module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
-import { HttpBackend, HttpClientModule, HttpInterceptor } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ForkableHttpClient } from './forkable-http-client';
-
-export function forkableHttpClientFactory(backend: HttpBackend, interceptors: HttpInterceptor[] | null = []): ForkableHttpClient {
-    return new ForkableHttpClient(backend, interceptors || []);
-}
 
 @NgModule({
     imports: [
