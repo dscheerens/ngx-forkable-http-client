d849d8d581feb54b5e6e111c6b85de23beb880d9
Daan Scheerens <d.scheerens@gmail.com>
2017-11-06 23:14:50 +0100

    Widen type of first constructor parameter for the `ForkableHttpClient` class from `HttpBackend` to `HttpHandler`

--- src/forkable-http-client.ts
+++ src/forkable-http-client.ts
@@ -1,17 +1,20 @@
 import { Injectable, Inject, Optional } from '@angular/core';
-import { HTTP_INTERCEPTORS, HttpBackend, HttpClient, HttpInterceptor } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpBackend, HttpClient, HttpHandler, HttpInterceptor } from '@angular/common/http';
 
 import { createInterceptorHandler } from './http-interceptor-handler';
 
 @Injectable()
 export class ForkableHttpClient extends HttpClient {
 
-    constructor(private backend: HttpBackend, @Optional() @Inject(HTTP_INTERCEPTORS) private interceptors: HttpInterceptor[]) {
-        super(createInterceptorHandler(backend, interceptors || []));
+    constructor(
+        @Inject(HttpBackend) private baseHandler: HttpHandler,
+        @Optional() @Inject(HTTP_INTERCEPTORS) private interceptors: HttpInterceptor[]
+    ) {
+        super(createInterceptorHandler(baseHandler, interceptors || []));
     }
 
     public fork(...interceptors: HttpInterceptor[]): ForkableHttpClient {
-        return new ForkableHttpClient(this.backend, [...this.interceptors, ...interceptors]);
+        return new ForkableHttpClient(this.baseHandler, [...this.interceptors, ...interceptors]);
     }
 
 }
