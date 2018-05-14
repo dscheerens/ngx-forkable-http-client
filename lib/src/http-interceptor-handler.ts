f62f2d0b3cd495643dbadc61cbd1cc2d2c29fef1
Daan Scheerens <d.scheerens@gmail.com>
2018-05-14 23:47:39 +0200

    feat: support Angular 6 and RxJS 6
    BREAKING CHANGES: This library now depends on **Angular** >=6.0.0, **RxJS** >=6.0.0 and **tslib** ^1.9.0

--- lib/src/http-interceptor-handler.ts
+++ lib/src/http-interceptor-handler.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 export function createInterceptorHandler(baseHandler: HttpHandler, interceptors: HttpInterceptor[]): HttpHandler {
     return interceptors.reduceRight((next, interceptor) => new HttpInterceptorHandler(next, interceptor), baseHandler);
@@ -8,7 +8,10 @@ export function createInterceptorHandler(baseHandler: HttpHandler, interceptors:
 
 export class HttpInterceptorHandler implements HttpHandler {
 
-    constructor(private next: HttpHandler, private interceptor: HttpInterceptor) { }
+    constructor(
+        private readonly next: HttpHandler,
+        private readonly interceptor: HttpInterceptor
+    ) { }
 
     public handle(request: HttpRequest<any>): Observable<HttpEvent<any>> {
         return this.interceptor.intercept(request, this.next);
